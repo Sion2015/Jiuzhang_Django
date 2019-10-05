9272aef80376f8f7bd5def59d47cea03b9d7899d
Songyang Guo <sit.songyang.guo@gmail.com>
2019-10-05 12:14:52 -0400

    Section 4: CRUD

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path("", HelloWorld.as_view(), name="home"),
+    path("test/", HelloWorld.as_view(), name="helloworld"),
+    path("posts/", PostView.as_view(), name="posts"),
+    path("post/<int:pk>", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="make_post"),
+    path("post/update/<int:pk>", PostUpdateView.as_view(), name="update_detail"),
+    path("post/delete/<int:pk>", PostDeleteView.as_view(), name="delete_detail"),
 ]
---
+++ README.md
@@ -0,0 +1,6 @@
+### update conda environment.yml
+https://stackoverflow.com/questions/49154899/resolvepackagenotfound-create-env-using-conda-and-yml-file-on-macos
+``` bash
+conda env export --no-builds > environment.yml
+```
+
